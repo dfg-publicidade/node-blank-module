307dd1194c9f11efa1952cf75918c930d6f9277a
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2020-12-01 09:43:21 -0300

    Inicializando repositório .git

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+declare -a copy=(".eslintrc.json" ".gitignore" "gulpfile.js" "tsconfig.json");   
+declare -a copyIfNotExists=("package.json" "src");
+
+blankdir="./blank-module";
+
+cd ../
+
+for dir in `find *-module -type d -depth 0 `; do
+    if [[ ${dir} != "blank-module" ]]; then
+        for file in "${copy[@]}"
+        do
+            echo $dir/$file;
+            rm -Rf $dir/$file && cp $blankdir/$file $dir
+        done
+
+        for file in "${copyIfNotExists[@]}"
+        do
+            if [ ! -f $dir/$file ] && [ ! -d $dir/$file ]; then
+                echo $dir/$file;
+                rm -Rf $dir/$file && cp -R $blankdir/$file $dir
+            fi
+        done
+    fi
+done

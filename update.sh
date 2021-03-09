e443a144755654e5a4a082282121247bcdcf88ae
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-09 11:49:09 -0300

    Alteração nas configurações de compilação

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 declare -a copyIfNotExists=("package.json" "src" "test");
-declare -a copy=(".eslintrc.json" ".gitignore" "gulpfile.js" "tsconfig.json", "test/tsconfig.json");   
+declare -a copy=(".eslintrc.json" ".gitignore" "gulpfile.js" "tsconfig.json" "test/tsconfig.json");   
 
 blankdir="./node-blank-module";
 
@@ -9,19 +9,23 @@ cd ../
 
 for dir in `find *-module -type d -depth 0 `; do
     if [[ ${dir} != "node-blank-module" ]]; then
+        echo $dir
+
         for file in "${copyIfNotExists[@]}"
         do
             if [ ! -f $dir/$file ] && [ ! -d $dir/$file ]; then
                 echo $dir/$file;
-                rm -Rf $dir/$file && cp -R $blankdir/$file $dir
+                rm -Rf $dir/$file && cp -R $blankdir/$file $dir/$file
             fi
         done
 
         for file in "${copy[@]}"
         do
             echo $dir/$file;
-            rm -Rf $dir/$file && cp $blankdir/$file $dir
+            rm -Rf $dir/$file && cp $blankdir/$file $dir/$file
             chmod -w $dir/$file
         done
+        
+        echo ''
     fi
 done
